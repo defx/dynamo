d62d2588f8c18a832a29786c6d9c89497993c7d7
defx <mdonkin83@gmail.com>
2022-11-11 18:03:26 +0000

    list delta

--- src/deriveSubscribers.js
+++ src/deriveSubscribers.js
@@ -3,9 +3,19 @@ import {
   BINDING_ATTRIBUTE_SELECTOR,
 } from "./constants.js"
 
+import { compareKeyedLists } from "./list.js"
+
 /*
 
 what if...there are two lists bound to the same path?
+
+perhaps you need to stick the x-bind on a container element?
+
+    - ideally, no - shouldn't need to restrict the html structure in that way
+
+    so then...you need to check siblings to determine whether nodes are part of the same collection
+
+    but if subscription is based on paths, then we would be putting that logic in the subscriber...at some point...later :)
 
 */
 
@@ -17,6 +27,15 @@ export function deriveSubscribers(rootNode, initialState) {
   for (const node of nodes) {
     const path = node.getAttribute(BINDING_ATTRIBUTE_NAME)
 
+    const { id } = node.dataset
+
+    if (!id) {
+      console.warn(
+        `list node with no data-id attribute. any changes to state will not be reflected in the DOM`,
+        node
+      )
+    }
+
     if (path.endsWith(".*")) {
       const k = path.slice(0, -2)
       if (byPath[k]) continue
@@ -27,7 +46,11 @@ export function deriveSubscribers(rootNode, initialState) {
         const newValue = state[k]
 
         if (newValue !== oldValue) {
-          // ...
+          const delta = compareKeyedLists("data-id", oldValue, newValue)
+
+          if (delta) {
+            // @todo: update the list...
+          }
 
           oldValue = newValue
         }
