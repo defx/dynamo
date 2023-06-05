9b23790a1767bd32809252936f46cd881d76489a
defx <mdonkin83@gmail.com>
2023-06-05 22:34:23 +0100

    types

---
+++ types/index.d.ts
@@ -0,0 +1,70 @@
+export = dynamo
+
+export as namespace dynamo
+
+declare namespace dynamo {
+  function Dynamo(rootNode: HTMLElement, configuration: DynamoConfig): void
+
+  type Primitive = string | boolean | number | null
+
+  type SerialisableObject = { [key: string]: Primitive | SerialisableObject }
+
+  type State = SerialisableObject
+
+  type StateUpdateFunction = {
+    (currentState: State, action: Action): State
+  }
+
+  type NodeProperties = {
+    value?: Primitive
+    textContent?: string
+  }
+
+  type NodeAttributes = {
+    [key: string]: string | { [key: string]: boolean }
+  }
+
+  type NodeConfig = NodeAttributes & NodeProperties
+
+  type NodeUpdateFunction = {
+    (currentState: State): NodeConfig
+  }
+
+  type DynamoConfig = {
+    state: SerialisableObject
+    action: { [key: string]: StateUpdateFunction }
+    node: { [key: string]: NodeUpdateFunction }
+    middleware?: { [key: string]: MiddlewareFunction }
+  }
+
+  type ActionInput = {
+    type: string
+    payload: {
+      [key: string]: any
+    }
+  }
+
+  type Action = {
+    type: string
+    payload?: SerialisableObject
+    event?: Event
+  }
+
+  type ActionHandler = {
+    (currentState: State, action: Action): State
+  }
+
+  type Store = {
+    getState(): State
+    dispatch(action: ActionInput): void
+  }
+
+  type MiddlewareFunction = {
+    /**
+     * A function that intercepts an action before it reaches its ActionHandler (if any),
+     * providing the opportunity to execute side effect(s),
+     * re-route actions, and dispatch new actions
+     */
+    (action: ActionInput, store: Store): void
+  }
+}
